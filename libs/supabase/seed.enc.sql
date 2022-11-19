46c711332ce41ca065af8f2cf16cb7cb1e4ad5ea
Thomas Newman <thomblweed@gmail.com>
2022-11-19 09:01:43 +0000

    user table created with seed setup

--- libs/supabase/seed.enc.sql
+++ libs/supabase/seed.enc.sql
@@ -1,5 +1,5 @@
 {
-	"data": "ENC[AES256_GCM,data:Z8mFnpxZ4G9fNILzUOm+vzxT8jLzxL6AeU8NRtL8aOd+HxdYsu0GgX6RydJO3pEeVYXhtRhzaPYa0QpgwnofSaoPJ3YvCyBrY7oEdizZraU4kRVqw7Q3Xwr7pBe5Cf4g3myBdtYaxGl9isrP0wGBmHN7I0G0HR7VxL/bMdRToubz8P9dZSCU58HyX+h8N3+mQ09WzdWFpTaV0tgiiLP+/ofpfMEi4IDHOqFIGvD3hdDwCDOzjstuI03/WN9UT5dwJykjFVpglFLAbcBdtDqmNIXFGY630P+AODofQm7aQy/WexqLc2xm52Br3htqoaR4CgzDuQkC6X0HusK2aidloWR74c5xmDrPnPIxWGyO0xw76+s8P5Ubqb/aglHTi3jXZZ6UufG6IlhuJCem8eVFw0aY1+5sXm2ECIlcC6TLw46qZ1fJ9w28e+N0c8JFy1qH1oCa/peWb7dF8ruhTOh0USa1JN4wWK4NxJsFxcssqdrHYUS28I241OeK+zqC5Pfhey8u5+1kdD7fV5LPBq8Z2q4jX2A83PrjHWmIEjN3wJpWShY7Q4zFY99B1GVUG6VOtYeanUwchgOxhbl26UC7sDCxoybBk7YdebvkV1yioyeKI8y1siOEB6mPQZ/+3/wXtkY0dxOatL0quFSVWxtdAA==,iv:AsYUEI4XUIgIxnsUfeXZ9EVs7HtdLcF3TjVXUOZkKBw=,tag:SAXT9KWXlYitDdpmqR5RxQ==,type:str]",
+	"data": "ENC[AES256_GCM,data:ibXEE04A6jmocKOn24S0r/S0uRf/QipXQ0dD9gPR7nIfw6QJbS61K+AACltfmTJcAjBbE3n+WKskMf2pTeoX2oNTjm3z+u1t0gObXwG1pOuNNR4e5c14njxGufFaqT919Zf6wcDxYtO9PoUkihNOedNQ0sN2S+w3PJ9v8ZxaguHqiURhEDhUdOG+/4ELCvpM9xx159W7yuRPw2xNWY+FlquzT1jiFzLE9c5pMD6vxy32dNQ/L1zd2rnr30/BfUizY88HJ/QnkJ/ShcYRKotjCUH8BAueO4KKMe1JN1OXshiITGHRjdrzlmvrUUMjT/o45TXvVkF4PWaUVq548qMxMEeXSlI0XDjCN3vfa49BLtZndHjk+7z/wmML8fMT6wzHgQ1ek79mQQtI9nezDexUmNa1pLV/s48C0K9TVcESAoV/7FqUfkXuq2skoNfwGzSY0cyPBx9LdHAl9LfLNptG+xq86A7eq4doRY6GLrBuVvMT04qX2qN14Wq1to32vAdsSl4I5jKCruZ8AZDrBolU503H1b5sV++YDh0ZP/LTNJWmwEA5QfoASty8C3bH91egrrAfqEDKxGvgJwybunCEwHShOofrO5QnQXJHhIDTQf0btZ1dFCkh3O+ngcQ8aWTmx8uSInnzQhA6F1ohHZjPkE9MY8pqt9buLTqma9MqTBQORFHsXwI2vW9EVfn9d612BpaoYif/nQ/lhSyq7rz+0JuvJn7OnHeyTDGi1Zyx03o+3hitSIoo7VoWZjRpD+p19SuvsngNu5ov7f1Q6pcldLM9cDq9dBWLJotDWJMu79AAzi+WOoL358wCQUq+MnK2NPv1y/2Dsbvwz1ftPBMjcmD6z92YQn/8+bwZnKDwBS2iKaveD/5WP4lZqXQGhOT9p9Ajthrt,iv:TIWgLIBDjALLFBXqZR7WYehHHIXRGyAUJ0K2Us882YU=,tag:CG2W+arvjcIY9VOS+Zf3eQ==,type:str]",
 	"sops": {
 		"kms": null,
 		"gcp_kms": null,
@@ -8,11 +8,11 @@
 		"age": [
 			{
 				"recipient": "age13wdxmgkryha5xznjjagayjs7rak2wu3msz77pjlkjj2hskhc7d3qq2d752",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBEdmVVRlp4aU5tcEVnOWRF\nREp6RDQyaTZMVkM3MVo2Uk8wNGFoRDdmVmtZCnJJY3g1clJtajF5OG9tRVdadFhO\nb3duYTVJUE5GNi8vczI1ZnlHcE5sN28KLS0tIEpDdDJjU0ZzcVhZTkc2dTgxRjdR\nYWdFaUpCV0tQSU5nbkRtblo5UXBRcmMKiK3pHdGasSlGlt3hw1aPAz8aPUOfw3qt\nOv92smme1u3WCL5JQnuKUxoqKkrBM2m7hbx2K3Sw4K7OXCJsrNS5sA==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBHdE1laUJHZ2FJWU1FbmRB\ncmp0U2RZbVpZclRDVWRldTV6M0x2ZDllT0FRCitBL0ZRSzNXL0J1UzVxMzZGNUJJ\nZnZZUTkyK2NqVWFVcnJxbnlKTVlyb0kKLS0tIE9WRHlZTk9hdVRaUWdvcnY5THBQ\ncjhIWmtMMUdRdmc1YTUzd3E3SWowU1kKBWlmmU59GgEcnacImhk+Sn3NNtYyMMQY\nmcs+smwc1SJVhtc3NyBgj6FF8ih7kahXfCD9wCtfapXKnnzLjNKJpg==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2022-11-17T20:39:58Z",
-		"mac": "ENC[AES256_GCM,data:NnKzIAvY3CT/ksCewACwo/fhCLlgq5d46p+b3+Zx0vHpf9/mMdKBz2TMmgc1XNBcYB1kjxDsAOPzSCOaVu+mKDA8Fh0WbUagE5tcNqxt3/iEd3H5fpNl08PLDrL1dxhbY0gdXvPnvtajsrGzgETX2vGgBZPCtnyPh7m+2rrcyqI=,iv:PhkaoAupsDQjRfERirC7emgOUeAkVn7q+Q/qBWwfym4=,tag:P6H1O41lvI6ab1XCqcFlIA==,type:str]",
+		"lastmodified": "2022-11-19T09:00:24Z",
+		"mac": "ENC[AES256_GCM,data:km/KlaecpR+8xyjLDuXh0E9cf+7sI6JrDIXK+lU2cmgioHVE6pbCsHoS/Kmbb5JCv7RrU8678bB5XM8HpJaOfYbPNfDsVvUF7EwumLFSs1zWPoDh1t92xmhXvuGJcG8jbSIm1wTvbhXgvFoDCsGPhZc7UExl2s6W3LUKVAH+B4U=,iv:FMW+vl/zOtMzIGlYU3Nt98JULNkt8jfrihepP/3yT80=,tag:6s1znGy3axChn+Dn0ZHcdg==,type:str]",
 		"pgp": null,
 		"unencrypted_suffix": "_unencrypted",
 		"version": "3.7.3"
