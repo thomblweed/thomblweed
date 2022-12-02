0c0c8bccd603d2d21f1218419df5d3d63647dd88
Thomas Newman <thomblweed@gmail.com>
2022-12-02 12:45:18 +0000

    minor setup role role routing

--- apps/client/app/routes/index.tsx
+++ apps/client/app/routes/index.tsx
@@ -1,33 +1,4 @@
-import type { LoaderFunction } from '@remix-run/node';
-import { json } from '@remix-run/node';
-
-import { getSupabaseClient } from '~/service/supabase.service';
-import type { Database } from '@types';
-
-type UserRoles = {
-  data: {
-    user_roles: Database['public']['Tables']['user_roles']['Row'];
-  } | null;
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
-  const response = new Response();
-  const supabase = getSupabaseClient(request, response);
-  const { data } = (await supabase
-    .from('user_profile')
-    .select(`user_roles(role)`)
-    .single()) as unknown as UserRoles;
-  if (data == null) {
-    return response;
-  }
-
-  const {
-    user_roles: { role },
-  } = data;
-  return json({ role });
-};
-
-export default function Index() {
+export default function Home() {
   return (
     <section>
       <p>
