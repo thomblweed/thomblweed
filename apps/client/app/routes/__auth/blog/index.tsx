a30d935625fa42d45a819eb480c4f7e5ab7a9ce7
Thomas Newman <thomblweed@gmail.com>
2023-01-29 15:25:44 +0000

    blogs list starter

--- apps/client/app/routes/__auth/blog/index.tsx
+++ apps/client/app/routes/__auth/blog/index.tsx
@@ -1,40 +1,40 @@
-import type { LinksFunction, LoaderFunction } from '@remix-run/node';
+import { Suspense } from 'react';
+import type { DataFunctionArgs, LinksFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useFetcher, useLoaderData } from '@remix-run/react';
+import { Await, useFetcher, useLoaderData } from '@remix-run/react';
 
 import { createSupabaseClient } from '~/service/supabase/supabase.service';
 import { Button, links as buttonStyles } from '~/components/Elements/Button';
 import type { Database } from '@types';
 
-type UserRoles = Database['public']['Tables']['user_roles']['Row'];
+type UserRole = Database['public']['Tables']['user_roles']['Row'];
 
 type UserData = {
   data: {
-    user_roles: UserRoles;
+    user_roles: UserRole;
   } | null;
 };
 
 export const links: LinksFunction = () => [...buttonStyles()];
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader = async ({ request }: DataFunctionArgs) => {
   const supabase = createSupabaseClient(request);
-  const { data } = (await supabase
+  // TODO: extract to service and promise all
+  const { data: roleData } = (await supabase
     .from('user_profile')
     .select(`user_roles(role)`)
     .limit(1)
-    .single()) as unknown as UserData;
+    .single()) as UserData;
+  const { data: blogsData } = await supabase.from('blogs').select('*');
 
-  if (data == null) {
-    return json({ role: 'user' });
-  }
-  const {
-    user_roles: { role }
-  } = data;
-  return json({ role });
+  return json({
+    role: roleData?.user_roles.role || 'user',
+    blogs: blogsData
+  });
 };
 
 export default function Blog() {
-  const { role } = useLoaderData<UserRoles>();
+  const { role, blogs } = useLoaderData<typeof loader>();
   const { submit, state } = useFetcher();
 
   const addBlog = () => {
@@ -54,6 +54,9 @@ export default function Blog() {
           Add Blog
         </Button>
       ) : null}
+      {blogs?.map((blog) => (
+        <div key={blog.id}>{blog.title}</div>
+      ))}
     </section>
   );
 }
