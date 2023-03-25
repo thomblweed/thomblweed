51d09733f3ef9947a3090e4da5019bc409ffc6db
Thomas Newman <thomblweed@gmail.com>
2023-03-25 10:34:35 +0000

    chore(client): saving wip

--- apps/client/app/root.tsx
+++ apps/client/app/root.tsx
@@ -1,7 +1,7 @@
 import type {
   ErrorBoundaryComponent,
   LinksFunction,
-  MetaFunction,
+  MetaFunction
 } from '@remix-run/node';
 import {
   Links,
@@ -9,7 +9,7 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from '@remix-run/react';
 import type { ReactNode } from 'react';
 
@@ -19,16 +19,24 @@ import fontStyles from '~/styles/font.css';
 import styles from '~/styles/styles.css';
 
 export const links: LinksFunction = () => [
+  {
+    rel: 'preload',
+    href: 'space-city.jpg',
+    imagesrcset:
+      '/images/space-city-480.jpg 480w, /images/space-city-960.jpg 960w, /images/space-city-1440.jpg 1440w, /images/space-city-1920.jpg 1920w, /images/space-city-2400.jpg 2400w',
+    as: 'image',
+    type: 'image/jpg'
+  },
   { rel: 'stylesheet', href: resetStyles },
   { rel: 'stylesheet', href: styles },
   { rel: 'stylesheet', href: fontStyles },
-  ...mainLayoutStyles(),
+  ...mainLayoutStyles()
 ];
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'thomblweed',
-  viewport: 'width=device-width,initial-scale=1',
+  viewport: 'width=device-width,initial-scale=1'
 });
 
 const Document = ({ children }: { children: ReactNode }) => (
