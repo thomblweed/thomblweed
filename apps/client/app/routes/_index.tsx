704bf9ef81bbb450ae8f22a362836e80eda7f9ef
thom <thomblweed@gmail.com>
2023-06-20 11:15:14 +0100

    about me updates 2

--- apps/client/app/routes/_index.tsx
+++ apps/client/app/routes/_index.tsx
@@ -10,8 +10,8 @@ export const links: LinksFunction = () => [
 
 export default function Home() {
   return (
-    <div className="about-container">
-      <Section>
+    <Section>
+      <div className="about-container">
         <picture className="profile-picture">
           <img
             src="/_static/images/thom.jpg"
@@ -19,17 +19,17 @@ export default function Home() {
             className="profile-img"
           />
         </picture>
-      </Section>
-      <Section>
-        <h2 className="about-me-text">about me</h2>
-        <p className="about-me-text">
-          Hello! I am an experienced frontend and full stack developer with a
-          passion for working in agile with cross-functional teams. With a solid
-          background in JavaScript, TypeScript, React, and Node, I am dedicated
-          to continuously expanding my knowledge and staying up-to-date with the
-          latest technologies.
-        </p>
-      </Section>
-    </div>
+        <div className="about-me">
+          <h2 className="about-me-text">about me</h2>
+          <p className="about-me-text">
+            Hello! I am an experienced frontend and full stack developer with a
+            passion for working in agile with cross-functional teams. With a
+            solid background in JavaScript, TypeScript, React, and Node, I am
+            dedicated to continuously expanding my knowledge and staying
+            up-to-date with the latest technologies.
+          </p>
+        </div>
+      </div>
+    </Section>
   );
 }
