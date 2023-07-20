ede5f0a6765fcfe3a906e2475b4b278cdc0727a4
thom <thomblweed@gmail.com>
2023-07-20 11:01:47 +0100

    styling updates for about me

--- apps/client/app/features/about-me/AboutMe.tsx
+++ apps/client/app/features/about-me/AboutMe.tsx
@@ -1,6 +1,9 @@
 import type { LinksFunction } from '@remix-run/node';
 
 import { Section, links as sectionStyles } from '~/components/Section';
+import { ReactRouterSVG } from '~/components/Svg/ReactRouterSVG';
+import { ReactSVG } from '~/components/Svg/ReactSVG';
+import { SolidSVG } from '~/components/Svg/SolidSVG';
 import aboutMeStyles from './about-me.css';
 import { Skills, links as skillsStyles } from './skills/Skills';
 
@@ -28,14 +31,18 @@ export const AboutMe = () => {
               className="profile-img"
             />
           </picture>
-          <div className="about-me center-text">
+          <div className="about-me">
             <h2>about me</h2>
             <p>
-              Frontend Developer, with a history of full stack development, over
-              8 years of industry experience including 5 years in consulting.
-              Thrives in agile environments, fostering collaboration and code
-              pairing. Demonstrates a strong passion for continuous learning and
-              has a proven track record of quickly adapting to new technologies.
+              Frontend Developer, with a history of full stack development and
+              over 8 years of industry experience including 5 years in
+              consulting. Thrives in agile environments, fostering collaboration
+              and code pairing. Demonstrates a strong passion for continuous
+              learning and has a proven track record of quickly adapting to new
+              technologies. Derives great satisfaction from thoroughly testing
+              across all areas, including unit, integration, acceptance, and
+              end-to-end, while utilising the principles of Test-Driven
+              Development (TDD).
             </p>
           </div>
         </div>
