ed27ce61f2d115ad6d7f21508c334a7f1d4ca027
thom <thomblweed@gmail.com>
2023-07-22 18:32:40 +0100

    unit testing setup with example

---
+++ apps/client/app/features/about-me/AboutMe.spec.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { AboutMe } from './AboutMe';
+
+describe('AboutMe', () => {
+  beforeEach(() => {
+    render(<AboutMe />);
+  });
+
+  it('should display about me heading 2', () => {
+    expect(
+      screen.getByRole('heading', { level: 2, name: 'about me' })
+    ).toBeInTheDocument();
+  });
+
+  it('should display skills heading 2', () => {
+    expect(
+      screen.getByRole('heading', { level: 2, name: 'skills' })
+    ).toBeInTheDocument();
+  });
+
+  it('should display about me paragraph', () => {
+    expect(
+      screen.getByText(
+        /Frontend Developer, with a history of full stack development and over 8 years of industry experience including 5 years in consulting. Thrives in agile environments, fostering collaboration and code pairing. Demonstrates a strong passion for continuous learning and has a proven track record of quickly adapting to new technologies. Derives great satisfaction from thoroughly testing across all areas, including unit, integration, acceptance, and end-to-end, while utilising the principles of Test-Driven Development \(TDD\)\./
+      )
+    ).toBeInTheDocument();
+  });
+});
