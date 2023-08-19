fbc90afd9bb7365285edd1d6fd61057c17925906
thom <thomblweed@gmail.com>
2023-08-19 15:18:27 +0100

    unit test fix

--- apps/client/test/setup-test-env.ts
+++ apps/client/test/setup-test-env.ts
@@ -1,5 +1,5 @@
 import { installGlobals } from '@remix-run/node';
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
 
 installGlobals();
