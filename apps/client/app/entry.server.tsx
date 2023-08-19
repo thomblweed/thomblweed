188d273e21f7b219f0a8b615e9dc630771aa3dd9
thom <thomblweed@gmail.com>
2023-08-19 15:08:30 +0100

    deps updates

--- apps/client/app/entry.server.tsx
+++ apps/client/app/entry.server.tsx
@@ -4,13 +4,13 @@
  * For more information, see https://remix.run/file-conventions/entry.server
  */
 
-import { PassThrough } from 'node:stream';
+import { PassThrough } from "node:stream";
 
-import type { AppLoadContext, EntryContext } from '@remix-run/node';
-import { Response } from '@remix-run/node';
-import { RemixServer } from '@remix-run/react';
-import isbot from 'isbot';
-import { renderToPipeableStream } from 'react-dom/server';
+import type { AppLoadContext, EntryContext } from "@remix-run/node";
+import { Response } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import isbot from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
 
 const ABORT_DELAY = 5_000;
 
@@ -21,7 +21,7 @@ export default function handleRequest(
   remixContext: EntryContext,
   loadContext: AppLoadContext
 ) {
-  return isbot(request.headers.get('user-agent'))
+  return isbot(request.headers.get("user-agent"))
     ? handleBotRequest(
         request,
         responseStatusCode,
@@ -55,12 +55,12 @@ function handleBotRequest(
           shellRendered = true;
           const body = new PassThrough();
 
-          responseHeaders.set('Content-Type', 'text/html');
+          responseHeaders.set("Content-Type", "text/html");
 
           resolve(
             new Response(body, {
               headers: responseHeaders,
-              status: responseStatusCode
+              status: responseStatusCode,
             })
           );
 
@@ -77,7 +77,7 @@ function handleBotRequest(
           if (shellRendered) {
             console.error(error);
           }
-        }
+        },
       }
     );
 
@@ -104,12 +104,12 @@ function handleBrowserRequest(
           shellRendered = true;
           const body = new PassThrough();
 
-          responseHeaders.set('Content-Type', 'text/html');
+          responseHeaders.set("Content-Type", "text/html");
 
           resolve(
             new Response(body, {
               headers: responseHeaders,
-              status: responseStatusCode
+              status: responseStatusCode,
             })
           );
 
@@ -126,7 +126,7 @@ function handleBrowserRequest(
           if (shellRendered) {
             console.error(error);
           }
-        }
+        },
       }
     );
 
