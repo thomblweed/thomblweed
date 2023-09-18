f1d6992a2035f288156b4b438e72b8fecad43cdd
thom <thomblweed@gmail.com>
2023-09-18 13:25:46 +0100

    updated to remix v2

---
+++ packages/client/.eslintrc.cjs
@@ -0,0 +1,18 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  root: true,
+  extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    '@remix-run/eslint-config/jest-testing-library',
+    'prettier'
+  ],
+  // We're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but it means we
+  // have to set the jest version explicitly.
+  settings: {
+    jest: {
+      version: 28
+    }
+  }
+};
---
+++ packages/client/remix.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  cacheDirectory: './node_modules/.cache/remix',
+  ignoredRouteFiles: ['**/.*', '**/*.test.{js,jsx,ts,tsx}'],
+  publicPath: '/_static/build/',
+  server: 'server.ts',
+  serverBuildPath: 'server/index.mjs',
+  serverModuleFormat: 'esm'
+};
