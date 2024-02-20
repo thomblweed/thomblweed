e72dcafc757a0ba072872f021553332a95333184
thom <thomblweed@gmail.com>
2024-02-20 20:38:44 +0000

    unit test fixes

--- packages/client/app/features/about-me/AboutMe.spec.tsx
+++ packages/client/app/features/about-me/AboutMe.spec.tsx
@@ -10,14 +10,14 @@ describe('AboutMe', () => {
 
   it('should display about me heading 2', () => {
     expect(
-      screen.getByRole('heading', { level: 2, name: 'about me' })
+      screen.getByRole('heading', { level: 2, name: "Hi, I'm Thom!" })
     ).toBeInTheDocument();
   });
 
   it('should display about me paragraph', () => {
     expect(
       screen.getByText(
-        /Frontend Developer, with a history of full stack development and over 9 years of industry experience including 5 years in consulting. Thrives in agile environments, fostering collaboration and code pairing. Demonstrates a strong passion for continuous learning and has a proven track record of quickly adapting to new technologies. Derives great satisfaction from thoroughly testing across all areas, including unit, integration, acceptance, and end-to-end, while utilising the principles of Test-Driven Development\./
+        /I'm a developer based in Edinburgh with over 9 years experience. Although my background encompasses fullstack development, I have recently been focussing in the frontend space. I have an enthusiasm for learning, and have a track record of adapting to new technologies./
       )
     ).toBeInTheDocument();
   });
