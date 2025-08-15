a4575cdca9dd3cab0086a16059bb437012fbcab1
thom <thomblweed@gmail.com>
2025-08-15 07:31:09 +0100

    latest config

--- packages/client/react-router.config.ts
+++ packages/client/react-router.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-    ssr: true,
-    serverBuildFile: 'index.js',
+  ssr: true,
+  serverBuildFile: 'index.js'
 } satisfies Config;
